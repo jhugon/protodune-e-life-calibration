49ac388281874efc3664252461e0336f68e1c3f3
Justin Hugon <5174443+jhugon@users.noreply.github.com>
2018-08-03 14:40:11 -0500

    trying to make like in analzyer

--- toyELifetime.py
+++ toyELifetime.py
@@ -10,8 +10,9 @@ lifetimeTrue = 2000. # us
 driftSpeed = 1 # us / tick
 trackSlope = 1 # tick / point
 landauWidth = qMPV*0.22
-ticksPerBin = 40
+ticksPerBin = 60
 nBins = nPoints // ticksPerBin
+msPerTick = 1.
 
 ts = numpy.zeros(nPoints)
 qTrues = numpy.zeros(nPoints)
@@ -41,7 +42,7 @@ for iPoint in range(nPoints):
 
     ## Now do the fit stuff
     iBin = t // ticksPerBin
-    if iBin < nBins:
+    if iBin < nBins and qMeas < 1500.:
       tck[iBin] += t
       ave[iBin] += qMeas
       err[iBin] += qMeas*qMeas
@@ -81,6 +82,72 @@ err /= numpy.sqrt(cnt)
 for iBin in range(nBins):
     print iBin, tck[iBin],ave[iBin],err[iBin],cnt[iBin]
 
+
+# fit to find the 1/lifetime
+Sum = 0.;
+sumx = 0.;
+sumy = 0.;
+sumxy = 0.;
+sumx2 = 0.;
+sumy2 = 0.;
+xx = 0.
+yy = 0. 
+wght = 0. 
+arg = 0.
+fitcnt = 0;
+
+logPlot_xs = []
+logPlot_ys = []
+  
+for ihist in range(nBins):
+  if(cnt[ihist] < 3):
+     continue;
+  if(err[ihist] == 0):
+    continue;
+  xx = (tck[ihist] - tck[0]) * msPerTick;
+  yy = numpy.log(ave[ihist]);
+  logPlot_xs.append(xx)
+  logPlot_ys.append(yy)
+  # error on log(x) = dx / x
+  arg = ave[ihist] / err[ihist];
+  wght = arg * arg;
+  Sum += wght;
+  sumx += wght * xx;
+  sumy += wght * yy;
+  sumx2 += wght * xx * xx;
+  sumxy += wght * xx * yy;
+  sumy2 += wght * yy * yy;
+  ++fitcnt;
+# calculate coefficients
+delta = Sum * sumx2 - sumx * sumx;
+print "delta ",delta
+A = (sumx2 * sumy - sumx * sumxy) / delta;
+print "A ", A
+# slope = 1 / lifetime
+B = (sumxy * Sum  - sumx * sumy) / delta;
+# calculate the error
+ndof = fitcnt - 2;
+varnce = (sumy2 + A*A*Sum + B*B*sumx2 - 2 * (A*sumy + B*sumxy - A*B*sumx)) / ndof;
+BErr = numpy.sqrt(varnce * Sum / delta);
+print "B ",B," ",BErr;
+
+lifeInv = -B;
+lifeInvErr = BErr / (B * B) ;
+
+# calculate chisq
+chi2 = 0;
+for ihist in range(nBins):
+  if(cnt[ihist] < 3):
+     continue;
+  if(err[ihist] == 0):
+     continue;
+  xx = (tck[ihist] - tck[0]) * msPerTick;
+  yy = numpy.exp(A - xx * lifeInv);
+  arg = (yy - ave[ihist]) / err[ihist];
+  chi2 += arg * arg;
+  print "chk ", ihist, " xx ", xx, " yy ", yy, " ave ", ave[ihist], " arg ", arg, "\n";
+chi2ndof = chi2 / ndof;
+
 ax.set_ylim(0,qMPV*8)
 ax.scatter(ts,qMeass,2.,c='k',lw=0)
 ax.plot(ts,qMPV*numpy.exp(-ts/lifetimeTrue),'-g')
@@ -90,3 +157,11 @@ ax.set_xlabel("Drift Time [us]")
 ax.set_ylabel("Charge")
 fig.savefig("Landau.png")
 fig.savefig("Landau.pdf")
+
+
+fig, ax = mpl.subplots()
+ax.plot(logPlot_xs,logPlot_ys,"bo")
+ax.set_xlabel("Drift Time [us]")
+ax.set_ylabel("log(Charge)")
+fig.savefig("LandauLog.png")
+fig.savefig("LandauLog.pdf")
